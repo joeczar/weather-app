8974695e0eda78ec83a0efa5368da18587f5ebdd
Joe Czarnecki <joe.czar@outlook.com>
2019-07-05 19:24:34 +0200

    Fixed typo

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ fetch(`/weather?location=${location}`).then((response) => {
       messageTwo.textContent = data.error;
     } else {
       messageOne.textContent = data.location;
-      messageTwo.textContent = `It is currently ${data.forecast.currentTemp} degrees with a daily high of ${data.forecast.dailyHigh} degrees and a low of ${data.forecast.dailyLow} degrees, with a ${data.forecast.percipitation}% chance of rain. ${data.forecast.dataDaily}`;
+      messageTwo.textContent = `It is currently ${data.forecast.currentTemp} degrees with a daily high of ${data.forecast.dailyHigh} degrees and a low of ${data.forecast.dailyLow} degrees. There is a ${data.forecast.percipitation}% chance of rain. ${data.forecast.dataDaily}`;
 
       console.log(data.forecast);
       
